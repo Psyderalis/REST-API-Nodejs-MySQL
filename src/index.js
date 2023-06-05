c89c7e48428f2f1ad245c2a2bb8520ff50c579cd
Melanie Tolvett S <melanietolvett@gmail.com>
2023-06-05 18:28:47 -0400

    organizando archivos index y app

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.routes.js'
-import indexRoutes from './routes/index.routes.js'
+// Aqui se inicializa la app
 
+import app from './app.js'
 import { PORT } from './config.js'
-
-const app = express()
-
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api', employeesRoutes)
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'endpoint not found'
-  })
-})
 
 app.listen(PORT)
 console.log(`Server running on port ${PORT}...`)
